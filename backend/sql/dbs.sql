c287fe902e00e7ac3c63f77517972d385378dedf
FengD <dingbobby@hotmail.com>
2022-11-12 20:40:30 +0800

    sdfdsf

--- backend/sql/dbs.sql
+++ backend/sql/dbs.sql
@@ -9,17 +9,17 @@ DROP TABLE IF EXISTS crdc_airi_detection_task;
 DROP TABLE IF EXISTS crdc_airi_tracking_task;
 DROP TABLE IF EXISTS crdc_airi_segmentation_task;
 
--- detection_task_id ,name,authors,description,test_dataset_url,model_files_url,pico_results_url,modalities,map,mate ,mase,maoe,mave,maae,nds,fps,result_details_url,state_report_url,nb_object_class
+-- name,authors,description,test_dataset_url,model_files_url,pico_results_url,modalities,map,mate ,mase,maoe,mave,maae,nds,fps,result_details_url,state_report_url,nb_object_class
 
 CREATE TABLE crdc_airi_detection_task(
   detection_task_id serial NOT NULL PRIMARY KEY,
-  name varchar(256),
-  authors varchar(256),
+  name varchar(256) NOT NULL,
+  authors varchar(256) NOT NULL,
   description varchar(1024),
   test_dataset_url varchar(512),
   model_files_url varchar(512),
   pico_results_url varchar(512),
-  modalities varchar(512),
+  modalities varchar(512) NOT NULL,
   map float DEFAULT 0,
   mate float DEFAULT 0,
   mase float DEFAULT 0,
@@ -37,13 +37,13 @@ CREATE TABLE crdc_airi_detection_task(
 
 CREATE TABLE crdc_airi_tracking_task(
   tracking_task_id serial NOT NULL PRIMARY KEY,
-  name varchar(256),
-  authors varchar(256),
+  name varchar(256) NOT NULL,
+  authors varchar(256) NOT NULL,
   description varchar(1024),
   test_dataset_url varchar(512),
   model_files_url varchar(512),
   pico_results_url varchar(512),
-  modalities varchar(512),
+  modalities varchar(512) NOT NULL,
   amota float DEFAULT 0,
   amotp float DEFAULT 0,
   motar float DEFAULT 0,
@@ -58,15 +58,16 @@ CREATE TABLE crdc_airi_tracking_task(
 
 CREATE TABLE crdc_airi_segmentation_task(
   segmentation_task_id serial NOT NULL PRIMARY KEY,
-  name varchar(256),
-  authors varchar(256),
+  name varchar(256) NOT NULL,
+  authors varchar(256) NOT NULL,
   description varchar(1024),
   test_dataset_url varchar(512),
   model_files_url varchar(512),
   pico_results_url varchar(512),
-  modalities varchar(512),
+  modalities varchar(512) NOT NULL,
   miou float DEFAULT 0,
   macc float DEFAULT 0,
+  mboundary float DEFAULT 0,
   fps float DEFAULT 0,
   result_details_url varchar(512),
   state_report_url varchar(512),
